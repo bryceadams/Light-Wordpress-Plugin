6fa8fd43adbfa0ab4b7ce4dc77daa726d07376b2
Bryce <brycead@gmail.com>
2014-09-17 12:33:28 +0700

    Allow 'fancybox' class to actually be a filterable array of classes

--- light.php
+++ light.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Light - Responsive LightBox
-Plugin URI: http://captaintheme.com/plugins/light/
+Plugin URI: http://bryceadams.com/
 Description: Automatically makes all images that link to other images into a responsive lightbox, using FancyBox from FancyApps.
 Author: Captain Theme
-Author URI: http://captaintheme.com
+Author URI: http://bryceadams.com/
 Version: 1.0
+License: GPL
 */
 
 /*
@@ -23,6 +24,12 @@ add_action('wp_enqueue_scripts', 'light_load_scripts');
 
 function light_load_inline_script() {
 	if ( wp_script_is( 'fancybox', 'done' ) ) {
+
+		// 'light_fancy_classes' Hook for easy filtering of the classes that fancybox gets applied to
+		$fancybox_class = apply_fitlers( 'light_fancy_classes', array(
+			'.fancybox';
+		) );
+
 		?>
 		<script type="text/javascript">
 		(function ($) {
@@ -34,8 +41,8 @@ function light_load_inline_script() {
 			// Make all images within a Wordpress gallery that link to an image into a lightbox gallery
 			$(".gallery a[href$='.jpg'],.gallery a[href$='.png'],.gallery a[href$='.gif'],.gallery a[href$='.jpeg']").attr('rel', 'gallery');
 
-			// Make all items with a class fancybox into a lightbox
-			$(".fancybox").fancybox();
+			// Make all items in the $fancybox_class var into a lightbox
+			$("<?php echo $fancybox_class; ?>").fancybox();
 
 		})(jQuery);
 		</script>
