83df823455380e96a0568e1b09be91557c099cec
Julien Vernet <julien@vernet.me>
2014-09-17 11:56:46 +0700

    Inline small javascript to improve page load time

--- light.php
+++ light.php
@@ -17,10 +17,32 @@ Version: 1.0
 function light_load_scripts() {
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'fancybox', plugin_dir_url( __FILE__ ) . 'js/jquery.fancybox.pack.js', array( 'jquery' ), false, true );
-	wp_enqueue_script( 'fancybox-load', plugin_dir_url( __FILE__ ) . 'js/light.js', array( 'fancybox' ), false, true );
 	wp_enqueue_style( 'fancybox-style', plugin_dir_url( __FILE__ ) . 'css/jquery.fancybox.css' );
 }
 add_action('wp_enqueue_scripts', 'light_load_scripts');
+
+function light_load_inline_script() {
+	if ( wp_script_is( 'fancybox', 'done' ) ) {
+		?>
+		<script type="text/javascript">
+		(function ($) {
+			'use strict';
+
+			// Make all images that link to images into a lightbox
+			$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif'],a[href$='.jpeg']").fancybox();
+
+			// Make all images within a Wordpress gallery that link to an image into a lightbox gallery
+			$(".gallery a[href$='.jpg'],.gallery a[href$='.png'],.gallery a[href$='.gif'],.gallery a[href$='.jpeg']").attr('rel', 'gallery');
+
+			// Make all items with a class fancybox into a lightbox
+			$(".fancybox").fancybox();
+
+		})(jQuery);
+		</script>
+		<?php
+	}
+}
+add_action( 'wp_footer', 'light_load_inline_script', 20 );
 
 
 /*
